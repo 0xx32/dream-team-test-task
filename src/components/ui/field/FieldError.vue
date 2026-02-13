197e566e5c21cb0ef98a082ef9f68f60d5cd0afd
0x32 <dimas1523@mail.ru>
2026-02-14 00:30:59 +0300

    feat: added Field ui component

---
+++ src/components/ui/field/FieldError.vue
@@ -0,0 +1,14 @@
+<template>
+	<p class="field__error">
+		<slot />
+	</p>
+</template>
+
+<style scoped lang="scss">
+.field__error {
+	font-weight: 400;
+	font-size: 0.75rem;
+	line-height: 1.4;
+	color: var(--semantic-danger);
+}
+</style>
---
+++ src/components/ui/field/FieldLabel.vue
@@ -0,0 +1,14 @@
+<template>
+	<label class="field__label">
+		<slot />
+	</label>
+</template>
+
+<style scoped lang="scss">
+.field__label {
+	font-weight: 400;
+	font-size: 0.875rem;
+	line-height: 1.4;
+	color: var(--neutral-600);
+}
+</style>
